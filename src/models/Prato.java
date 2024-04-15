edf4d9c6fbef5694fc00e451d5c8effe064e1f54
Internautilo <yuribertulucci2004@gmail.com>
2024-04-15 20:12:58 -0300

    feat: Add get methods to Prato

--- src/models/Prato.java
+++ src/models/Prato.java
@@ -3,7 +3,19 @@ package models;
 import java.time.LocalDate;
 
 public class Prato {
-	protected Float price;
-	protected LocalDate expireDate;
-	protected Float weight;
+	protected double price;
+	protected LocalDate expirationDate;
+	protected double weight;
+	
+	public double getPrice() {
+		return this.price;
+	}
+	
+	public double getWeight() {
+		return this.weight;
+	}
+	
+	public LocalDate getExpirationDate() {
+		return this.expirationDate;
+	}
 }
