c706ab85cec8b04ec43a04848cc841be7e4bea83
Internautilo <yuribertulucci2004@gmail.com>
2024-04-20 17:48:53 -0300

    feat: main is now used to start the program.

--- src/main/main.java
+++ src/main/main.java
@@ -3,13 +3,15 @@ package main;
 import java.sql.Connection;
 
 import Database.DBConnection;
+import panels.MainFrame;
 
 public class main {
 
 	public static void main(String[] args) {
 		
-		DBConnection dbconn = new DBConnection();
-		Connection conn = dbconn.getConnection();
+		MainFrame main = new MainFrame();
+		main.Start();
+		
 	}
 
 }
